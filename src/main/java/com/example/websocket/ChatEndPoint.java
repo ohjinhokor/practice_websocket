3fb1ef9ea8e29e323c7e7cb760801aaad4b138c4
ohjinhokor <ohjinho7@gmail.com>
2022-03-23 22:35:36 +0900

    Baeldung - 골격 메서드 구현

--- src/main/java/com/example/websocket/ChatEndPoint.java
+++ src/main/java/com/example/websocket/ChatEndPoint.java
@@ -1,6 +1,7 @@
 package com.example.websocket;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.HashMap;
@@ -15,22 +16,48 @@ public class ChatEndPoint {
     private static HashMap<String, String> users = new HashMap<>();
 
     @OnOpen
-    public void onOpen(Session session) throws IOException{
+    public void onOpen(Session session, @PathParam("username") String username) throws IOException, EncodeException{
 
+        this.session = session;
+        chatEndPoints.add(this);
+        users.put(session.getId(), username);
+
+        Message message = new Message();
+        message.setFrom(username);
+        message.setContent("Connected");
+        broadcast(message);
     }
 
     @OnMessage
-    public void onMessage(Session session, String message) throws IOException{
-
+    public void onMessage(Session session, Message message) throws IOException, EncodeException{
+        message.setFrom(users.get(session.getId()));
+        broadcast(message);
     }
 
     @OnClose
-    public void onClose(Session session) throws IOException{
-
+    public void onClose(Session session) throws IOException, EncodeException{
+        chatEndPoints.remove(this);
+        Message message = new Message();
+        message.setFrom(users.get(session.getId()));
+        message.setContent("Disconnected!");
+        broadcast(message);
     }
 
     @OnError
     public void onError(Session session, Throwable throwable){
 
+    }
+
+    private static void broadcast(Message message) throws IOException, EncodeException {
+        chatEndPoints.forEach(endpoint -> {
+            synchronized (endpoint) {
+                try {
+                    endpoint.session.getBasicRemote().
+                            sendObject(message);
+                } catch (IOException | EncodeException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
